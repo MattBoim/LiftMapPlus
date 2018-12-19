bcc721ac611d430635e3b2ada471271e74d8b1c8
Matt Rozenboim <mattrozenboim@gmail.com>
2018-12-19 12:22:58 -0800

    Added stevens pass map

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,13 +12,14 @@ namespace LiftMaps.Controllers
     public class HomeController : Controller
     {
         static string _address = "https://liftie.info/api/resort/whistler-blackcomb";
+        static string _address2 = "https://liftie.info/api/resort/stevens";
         
         // GET: /Home/
         [HttpGet]
-        [Route("")]
+        [Route("/whistler")]
         public async Task<IActionResult> Index()
         {
-            var res = await GetExternalResponse();
+            var res = await GetExternalResponse(_address);
             JObject json = JObject.Parse(res);
             // var status =  Get();
             System.Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!");
@@ -28,12 +29,27 @@ namespace LiftMaps.Controllers
             ViewBag.stats = json["lifts"]["stats"];
             ViewBag.weather = json["weather"]["text"];
             ViewBag.conditions = json["weather"]["conditions"];
+            return View("Index");
+        }
+
+        [HttpGet("/stevens")]
+        public async Task<IActionResult> Stevens()
+        {
+            var res = await GetExternalResponse(_address2);
+            JObject json = JObject.Parse(res);
+            System.Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!");
+            System.Console.WriteLine("STEVENS RES: "+json);
+            System.Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!");
+            ViewBag.status = json["lifts"]["status"];
+            ViewBag.stats = json["lifts"]["stats"];
+            ViewBag.weather = json["weather"]["text"];
+            ViewBag.conditions = json["weather"]["conditions"];
             return View();
         }
-        private async Task<string> GetExternalResponse()
+        private async Task<string> GetExternalResponse(string address)
         {
             var client = new HttpClient();
-            HttpResponseMessage response = await client.GetAsync(_address);
+            HttpResponseMessage response = await client.GetAsync(address);
             response.EnsureSuccessStatusCode();
             var result = await response.Content.ReadAsStringAsync();
             return result;
